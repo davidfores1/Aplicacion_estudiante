41263ca99ccbaacd630215cc9220d7dab5dc1318
David Forero <davidforero717@gmail.com>
2021-02-18 13:21:27 -0500

    Se realiza pruebas con SoapUI validando con exito las funciones de crud Matriculas

--- WebServiceSOAP/src/java/modelo/Matriculas.java
+++ WebServiceSOAP/src/java/modelo/Matriculas.java
@@ -1,13 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 import java.io.Serializable;
-import java.util.Date;
-import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,12 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -30,26 +19,18 @@ import javax.xml.bind.annotation.XmlRootElement;
 @Entity
 @Table(name = "matriculas")
 @XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Matriculas.findAll", query = "SELECT m FROM Matriculas m")
-    , @NamedQuery(name = "Matriculas.findByIdMatricula", query = "SELECT m FROM Matriculas m WHERE m.idMatricula = :idMatricula")
-    , @NamedQuery(name = "Matriculas.findByFecha", query = "SELECT m FROM Matriculas m WHERE m.fecha = :fecha")})
+
 public class Matriculas implements Serializable {
 
-    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "idMatricula")
-    private Integer idMatricula;
-    @Basic(optional = false)
-    @NotNull
-    @Column(name = "fecha")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date fecha;
+    private int idMatricula;
+ 
     @JoinColumn(name = "idEstudiante", referencedColumnName = "idEstudiante")
     @ManyToOne(optional = false)
     private Estudiantes idEstudiante;
+    
     @JoinColumn(name = "idMateria", referencedColumnName = "idMateria")
     @ManyToOne(optional = false)
     private Materias idMateria;
@@ -57,31 +38,20 @@ public class Matriculas implements Serializable {
     public Matriculas() {
     }
 
-    public Matriculas(Integer idMatricula) {
+    public Matriculas(int idMatricula) {
         this.idMatricula = idMatricula;
     }
 
-    public Matriculas(Integer idMatricula, Date fecha) {
-        this.idMatricula = idMatricula;
-        this.fecha = fecha;
-    }
-
-    public Integer getIdMatricula() {
+    public int getIdMatricula() {
         return idMatricula;
     }
 
-    public void setIdMatricula(Integer idMatricula) {
+    public void setIdMatricula(int idMatricula) {
         this.idMatricula = idMatricula;
     }
 
-    public Date getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(Date fecha) {
-        this.fecha = fecha;
-    }
-
+    
+    
     public Estudiantes getIdEstudiante() {
         return idEstudiante;
     }
@@ -98,29 +68,4 @@ public class Matriculas implements Serializable {
         this.idMateria = idMateria;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (idMatricula != null ? idMatricula.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Matriculas)) {
-            return false;
-        }
-        Matriculas other = (Matriculas) object;
-        if ((this.idMatricula == null && other.idMatricula != null) || (this.idMatricula != null && !this.idMatricula.equals(other.idMatricula))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "modelo.Matriculas[ idMatricula=" + idMatricula + " ]";
-    }
-    
 }
